d86004b44b4257637a4b2ab2482121a8e3a92cec
Boris Yue <borisyue@yahoo.com>
2015-07-01 16:37:47 -0700

    jump with jumping fixed(equal jumps)

---
+++ Jump.spritebuilder/Source/Hero.swift
@@ -0,0 +1,23 @@
+//
+//  Hero.swift
+//  Jump
+//
+//  Created by Boris Yue on 7/1/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import UIKit
+
+class Hero: CCSprite {
+   
+    func jumpUpAnimation(){
+        self.animationManager.runAnimationsForSequenceNamed("jumpupanimation")
+    }
+//    func jumpUp(){
+//        self.animationManager.runAnimationsForSequenceNamed("jumpup")
+//
+//    }
+    func down(){
+        self.animationManager.runAnimationsForSequenceNamed("down")
+    }
+}
--- Jump.spritebuilder/Source/MainScene.swift
+++ Jump.spritebuilder/Source/MainScene.swift
@@ -1,5 +1,47 @@
 import Foundation
+import SpriteKit
 
 class MainScene: CCNode {
 
+    weak var scoreLabel : CCLabelTTF!
+    weak var gamePhysicsNode : CCPhysicsNode!
+    weak var hero : Hero!
+    var gameOver = false
+    var startedJumping = false
+    var firstPoint: CGPoint?
+    var lineNode = SKShapeNode()
+    
+    func didLoadFromCCB(){
+        gamePhysicsNode.collisionDelegate = self
+        userInteractionEnabled = true
+    }
+    override func update(delta: CCTime){
+        if(hero.physicsBody.velocity.y < 0){
+            hero.down()
+        }
+      
+    }
+    func ccPhysicsCollisionBegin(pair: CCPhysicsCollisionPair!, monster: CCSprite!, ground: CCPhysicsNode!) -> Bool {
+
+        hero.physicsBody.velocity = ccp(0,500)
+        hero.jumpUpAnimation()
+        return true
+    }
+    override func touchBegan(touch: CCTouch!, withEvent event: CCTouchEvent!) {
+        if (gameOver == false) {
+            var worldPoint : CGPoint = touch.locationInWorld()
+            firstPoint = touch.locationInNode(self)
+        }
+    }
+    override func touchMoved(touch: CCTouch!, withEvent event: CCTouchEvent!) {
+        let path = CGPathCreateMutable()
+        var newPos = touch.locationInNode(self)
+        CGPathMoveToPoint(path, nil, firstPoint!.x , firstPoint!.y)
+        CGPathAddLineToPoint(path, nil, newPos.x, newPos.y)
+        lineNode.path = path
+        lineNode.lineWidth = 20
+        lineNode.strokeColor = UIColor.blackColor()
+        firstPoint = newPos
+    }
+    
 }
